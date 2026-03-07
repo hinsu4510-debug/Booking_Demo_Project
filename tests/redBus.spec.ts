3464dd70d8d6fb6bd16b57f4587c3ec6b540a885
Himanshu <hinsu4510@gmail.com>
2026-03-07 23:58:32 +0530

    hii

--- tests/redBus.spec.ts
+++ tests/redBus.spec.ts
@@ -1,7 +1,7 @@
 import{test,expect} from '@playwright/test';
 import { RedBus } from '../Pages/RedBusPage';
 
-test("redBus datepicker ",async({page})=>{
+test("redBus datepickerr ",async({page})=>{
 let RedBuspage=new RedBus(page);
 await RedBuspage.navigate();
 
